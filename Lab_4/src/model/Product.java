2c319a3417fb0a8bf1a248272f36354cb53f32c4
Reshma Savarirajan <savarirajan.r@northeastern.edu>
2023-11-05 22:59:00 -0400

    Updating Lab_4

---
+++ Lab_4/src/model/Product.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author reshma
+ */
+public class Product {
+     private String name;
+    private int price;
+    private int id;
+    
+    private ArrayList<Feature> features;
+
+    private static int count = 0;
+    
+    public Product() {
+        count++;
+        id = count;
+        features = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public Feature addNewFeature() {
+        
+        Feature f = new Feature(this);
+        features.add(f);
+        return f;
+    }
+
+}
